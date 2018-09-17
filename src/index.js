2b4d34d17701fe58855a4cf1718104d8b28f5bd6
DevandScorp <artemda459@gmail.com>
2018-09-17 16:39:16 +0300

    additional_5:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var stack = [];
+    var flag;
+    stack.push(str.charAt(0));
+    for(let i = 1;i<str.length;++i){
+        flag = true;
+        for(let bracket of bracketsConfig){
+            if(stack[stack.length-1]===bracket[0] && str.charAt(i)===bracket[1]){
+                stack.pop();
+                flag = false;
+                break;
+            }
+
+        }
+        if(flag)stack.push(str.charAt(i));
+    }
+    return stack.length === 0;
+
 }
